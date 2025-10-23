3358355f1e6e9a276c0b8c9c3eab964f89e3a18c
gcgbarbosa <gcgbarbosa@gmail.com>
2025-10-23 15:06:26 +0200

    feat: enhance AI-Cares application by integrating chat functionality and timer
    - Added logger for application initialization
    - Replaced MessagesComponent with ChatbotsContainerComponent
    - Introduced TimerModel to manage countdowns
    - Implemented UI timer to notify when the application is not ready
    - Adjusted layout for the chat container and footer visibility based on timer
    - Updated app entry point to support new functionality

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 """Entrypoint"""
 
-from nicegui import app, ui
-from components import HeaderComponent, FooterComponent, MessagesComponent
+from loguru import logger
+from nicegui import ui
+
+from components import ChatbotsContainerComponent, FooterComponent, HeaderComponent
+
+from src.controllers import TimerModel
+
+logger.info("Initializing AI-Cares application")
 
 
 @ui.page("/")
@@ -16,13 +22,49 @@ def main():
         int: The result of the arithmetic operation (1 + 1), which is 2.
     """
 
-    HeaderComponent()
+    TIME_PER_STEP = 5
+    elapsed_time = 0
+    timer_model = TimerModel(max(0, TIME_PER_STEP - elapsed_time))
 
-    MessagesComponent()
+    timer = ui.timer(1.0, callback=lambda: ui.notify("The application is not ready yet"), active=False)
 
-    FooterComponent()
+    ui.page_title("AI-Cares")
+
+    HeaderComponent(timer_model, timer)
+
+    chat_container = ChatbotsContainerComponent()
+
+    # chat_container.get_message_container(3)
+
+    footer = FooterComponent(chat_container)
+
+    footer._btn_input_chat.enable()
+    footer._txt_input_chat.enable()
+
+    def countdown():
+        has_time_left = timer_model.remaining > 0
+
+        if has_time_left:
+            timer_model.remaining -= 1
+            return
+
+        timer.active = False
+
+        footer.element.hide()
+
+        chat_container.element.classes.remove("absolute-full")
+        chat_container.element.classes.append("h-150")
+
+        ui.run_javascript("window.scrollTo(0, document.body.scrollHeight)")
+
+    timer.callback = countdown
+
+    # timer.active = True
 
 
 if __name__ in {"__main__", "__mp_main__"}:
-
-    ui.run(storage_secret="THIS_NEEDS_TO_BE_CHANGED", port=8880)
+    ui.run(
+        storage_secret="THIS_NEEDS_TO_BE_CHANGED",
+        port=8880,
+        favicon="https://thomasmore.be/themes/custom/tm/assets/images/favicon/favicon.ico",
+    )
