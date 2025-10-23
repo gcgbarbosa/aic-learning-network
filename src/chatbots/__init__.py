409888559843b290d0671168ba36efb4fcef4350
gcgbarbosa <gcgbarbosa@gmail.com>
2025-10-23 15:06:35 +0200

    feat: add ChatbotFactory and integrate PocketBaseDB
    - Imported ChatbotFactory to manage chatbot instances.
    - Integrated PocketBaseDB to list chatbots and ensure database synchronization.

--- src/chatbots/__init__.py
+++ src/chatbots/__init__.py
@@ -1,5 +1,8 @@
 from .base_chatbot import BaseChabot
 from .chatbot import Chatbot
+from .factory import ChatbotFactory
+
+from src.pocketbase_db import PocketBaseDB
 
 # from .fewshot import FewshotChatbot
 # from .rag import RAGChatbot
@@ -8,4 +11,9 @@ from .chatbot import Chatbot
 #
 #
 # __all__ = ["BaseChabot", "FewshotChatbot", "RAGChatbot", "ZeroshotChatbot"]
-__all__ = ["BaseChabot", "Chatbot"]
+__all__ = ["BaseChabot", "Chatbot", "ChatbotFactory"]
+
+
+db = PocketBaseDB()
+chatbots_list = db.list_chatbots()
+ChatbotFactory.is_database_in_sync(chatbots_list)
