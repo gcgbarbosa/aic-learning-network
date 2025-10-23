d76d2f182c66c350dbb2c0bacddc7313668cc1d9
gcgbarbosa <gcgbarbosa@gmail.com>
2025-10-23 15:07:40 +0200

    feat(header): add timer functionality and control buttons
    - Integrated TimerModel and Timer into HeaderComponent for timing functionality.
    - Added buttons to start/pause and end the conversation, updating visibility based on remaining time.
    - Implemented time formatting for the countdown display.

--- src/components/header.py
+++ src/components/header.py
@@ -1,27 +1,49 @@
 from nicegui import ui
 
+from datetime import timedelta
+
+from nicegui.timer import Timer
+
+from src.controllers.timer import TimerModel
+
 
 class HeaderComponent:
-    def __init__(self):
+    def __init__(self, timer_model: TimerModel, timer: Timer):
         # name = app.storage.user.get("name", "User")
+        self._timer = timer
 
         with ui.header().classes("items-center"):
-            # with ui.row().classes("items-center w-full max-w-3xl mx-auto"):
-            ui.label("AI-Cares")
-
-            ui.space()
-            # ui.label(f"{name}")
-            # ui.icon("Testing interface")
-            ui.button(text="Submit feedback", on_click=lambda: ui.notify("Feedback submited"), icon="feedback").props(
-                "flat color=white"
+            ui.label().bind_text_from(timer_model, "remaining", backward=self.format_time_left).classes(
+                "text-lg font-bold"
             )
 
-    # TODO: this must be moved to the footer
-    def prevent_new_line_on_enter(self):
-        ui.run_javascript("""
-            document.addEventListener("keydown", function (event) {
-                if (event.key === "Enter" && !event.shiftKey) {
-                    event.preventDefault();  // stop newlines or form submissions
-                }
-            });
-        """)
+            self._start_btn = ui.button(text="Start conversation", on_click=self.toggle_timer).props(
+                "outline color=white"
+            )
+            self._start_btn.bind_visibility_from(timer_model, "remaining", backward=lambda v: v > 0)
+
+            self._config_btn = (
+                ui.button(icon="settings", on_click=lambda: ui.notify("Config clicked"))
+                .classes("outlined")
+                .props("outline color=white ")
+            )
+            self._config_btn.bind_visibility_from(timer_model, "remaining", backward=lambda v: v > 0)
+
+            ui.space()
+
+            ui.button(text="End conversation", on_click=lambda: ui.notify("End conversation"), icon="close").props(
+                "outline color=white"
+            )
+
+    def toggle_timer(self):
+        if self._timer.active:
+            self._timer.deactivate()
+            self._start_btn.set_text("Pause conversation")
+
+        else:
+            self._timer.activate()
+            self._start_btn.set_text("Stop conversation")
+
+    def format_time_left(self, seconds: int) -> str:
+        """Format countdown seconds into mm:ss string (skip hours)."""
+        return "Time left: " + str(timedelta(seconds=seconds))[2:]
