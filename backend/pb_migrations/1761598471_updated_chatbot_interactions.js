0409241caa0b831cae349199c56214164fad622c
gcgbarbosa <gcgbarbosa@gmail.com>
2025-10-27 22:03:00 +0100

    feat(migrations): add chatbot interaction settings fields to collections
    - Introduced new migration scripts to update chatbot interactions.
    - Added `interaction_setting_id` and `interaction_settings_id` fields to the collection with appropriate configurations (relation type, visibility, and required attributes).
    - Ensured backward migration setup for the new fields to maintain database integrity.

---
+++ backend/pb_migrations/1761598471_updated_chatbot_interactions.js
@@ -0,0 +1,40 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_95463030")
+
+  // update field
+  collection.fields.addAt(5, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_3535628324",
+    "hidden": false,
+    "id": "relation280719904",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "interaction_settings_id",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_95463030")
+
+  // update field
+  collection.fields.addAt(5, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_3535628324",
+    "hidden": false,
+    "id": "relation280719904",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "interaction_settings_id",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+})
