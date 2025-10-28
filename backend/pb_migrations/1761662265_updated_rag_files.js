a8b47823ab5c963ab7b5ba483216d887674ba5dc
gcgbarbosa <gcgbarbosa@gmail.com>
2025-10-28 19:17:56 +0100

    feat(migrations): add multiple chatbot interaction and settings migration files
    - Added migration scripts to update chatbot interaction collections, including
      rules for listing and viewing.
    - Introduced new field "chatbot_settings_id" to the collection with relation
      configurations.
    - Scripts handle both application and rollback actions for all changes.

---
+++ backend/pb_migrations/1761662265_updated_rag_files.js
@@ -0,0 +1,40 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_870893133")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_3508435212",
+    "hidden": false,
+    "id": "relation3480340361",
+    "maxSelect": 999,
+    "minSelect": 0,
+    "name": "chatbot_settings_id",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_870893133")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_3508435212",
+    "hidden": false,
+    "id": "relation3480340361",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "chatbot_settings_id",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+})
